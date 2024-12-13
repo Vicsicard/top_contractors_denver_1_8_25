37e2a174c8256b98e7d7a095606110f8fee6941d
Vicsicard <vicsicard@gmail.com>
2024-12-12 20:47:25 -0700

    fix: update serverActions config in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ["*"],
+      bodySizeLimit: "2mb"
+    },
   },
   webpack: (config) => {
     // Handle MongoDB driver issue
